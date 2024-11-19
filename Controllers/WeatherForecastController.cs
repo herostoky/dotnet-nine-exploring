9175a3c844b52fbfbd846e44a0225e4a6790cf6c
Toky SOLOFONIRINA <herostoky@gmail.com>
2024-11-19 09:47:42 +0300

    fix: Change minimal API to Controller

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MigrationFix.Models;
+
+namespace MigrationFix.Controllers;
+
+[Controller]
+[ApiController]
+[Route("api/[controller]")]
+public class WeatherForecastController: ControllerBase
+{
+    private readonly string[] _summaries =
+        ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
+    [HttpGet(Name = nameof(GetWeatherForecast))]
+    public IEnumerable<WeatherForecast> GetWeatherForecast()
+    {
+        var forecast =  Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    _summaries[Random.Shared.Next(_summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    }
+}
