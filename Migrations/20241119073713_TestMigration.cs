41e090cdcf43f5835f14f204fb353121bda07b05
Toky SOLOFONIRINA <herostoky@gmail.com>
2024-11-19 11:00:15 +0300

    feat: Add migration for test

---
+++ Migrations/20241119073713_TestMigration.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MigrationFix.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FullName = table.Column<string>(type: "text", nullable: true),
+                    CustomFieldsValue = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LeaveType",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Type = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LeaveType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LeaveRequest",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    _employeeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    _leaveTypeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreateDateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    From = table.Column<DateOnly>(type: "date", nullable: false),
+                    To = table.Column<DateOnly>(type: "date", nullable: false),
+                    ApprovalStatus = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LeaveRequest", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LeaveRequest_Employee__employeeId",
+                        column: x => x._employeeId,
+                        principalTable: "Employee",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LeaveRequest_LeaveType__leaveTypeId",
+                        column: x => x._leaveTypeId,
+                        principalTable: "LeaveType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LeaveRequestDays",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Date = table.Column<DateOnly>(type: "date", nullable: false),
+                    Duration = table.Column<string>(type: "text", nullable: false),
+                    StartTime = table.Column<TimeOnly>(type: "time without time zone", nullable: false),
+                    EndTime = table.Column<TimeOnly>(type: "time without time zone", nullable: false),
+                    LeaveRequestId = table.Column<Guid>(type: "uuid", nullable: true),
+                    _leaveRequestId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LeaveRequestDays", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LeaveRequestDays_LeaveRequest_LeaveRequestId",
+                        column: x => x.LeaveRequestId,
+                        principalTable: "LeaveRequest",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_LeaveRequestDays_LeaveRequest__leaveRequestId",
+                        column: x => x._leaveRequestId,
+                        principalTable: "LeaveRequest",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveRequest__employeeId",
+                table: "LeaveRequest",
+                column: "_employeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveRequest__leaveTypeId",
+                table: "LeaveRequest",
+                column: "_leaveTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveRequestDays__leaveRequestId",
+                table: "LeaveRequestDays",
+                column: "_leaveRequestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveRequestDays_LeaveRequestId",
+                table: "LeaveRequestDays",
+                column: "LeaveRequestId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LeaveRequestDays");
+
+            migrationBuilder.DropTable(
+                name: "LeaveRequest");
+
+            migrationBuilder.DropTable(
+                name: "Employee");
+
+            migrationBuilder.DropTable(
+                name: "LeaveType");
+        }
+    }
+}
